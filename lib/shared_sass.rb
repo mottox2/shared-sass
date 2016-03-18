025837c0dce68ebfb2525b1dcedfdf0a9c0e431e
mottox2 <y.mottox2@gmail.com>
2016-03-19 02:49:46 +0900

    Update 0.0.2

--- lib/shared_sass.rb
+++ lib/shared_sass.rb
@@ -3,4 +3,8 @@ require "sass"
 
 module SharedSass
   Sass.load_paths << File.expand_path("../../app/assets/stylesheets", __FILE__)
+  if defined?(Rails)
+    class Engine < Rails::Engine
+    end
+  end
 end
